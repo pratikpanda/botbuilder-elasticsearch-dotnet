8eafb5a87fafed8d5f117bd124f988934d8af274
Pratik Panda <mailtopratikpanda@gmail.com>
2018-12-10 09:49:24 +0530

    Fixed typo.

--- Build.ps1
+++ Build.ps1
@@ -3,7 +3,7 @@
 	1. If found, clear out any existing artifacts.
     2. Restore the required NuGet packages.
 	3. Build the solution in Release mode.
-	4. Package all the artifacts (NuGet packages) and put them in the artifaces directory.
+	4. Package all the artifacts (NuGet packages) and put them in the artifacts directory.
 #>
 
 if(Test-Path .\artifacts) { Remove-Item .\artifacts -Force -Recurse }
